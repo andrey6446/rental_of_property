4c5f9c397c1bcf716c013fd69b9441ea994b862a
andrey6446 <andrey6446@yandex.ru>
2024-05-01 22:02:47 +0300

    feat:navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,19 @@
 const React = require('react');
+const NavBar = require('./NavBar');
 
 function Layout({ title, children, user }) {
   return (
     <html lang='ru'>
       <head>
         <title>{title}</title>
+        <link rel='icon' type='image/x-icon' href='/images/favicon.png' />
+        <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js' />
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js' />
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' />
+        <link rel='stylesheet' href='/styles/style.css' />
       </head>
       <body>
+        <NavBar user={user} />
         {children}
       </body>
     </html>
